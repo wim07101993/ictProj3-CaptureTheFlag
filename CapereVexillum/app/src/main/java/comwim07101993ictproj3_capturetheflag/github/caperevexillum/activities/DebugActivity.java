d4cbcce4c576949e81fed014d07e6ba4d5b7e019
DESKTOP-DIID4P1\Hakan <sanlee75@hotmail.com>
2017-10-25 18:13:51 +0200

    adding debug page

---
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/activities/DebugActivity.java
@@ -0,0 +1,15 @@
+package comwim07101993ictproj3_capturetheflag.github.caperevexillum.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.R;
+
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
