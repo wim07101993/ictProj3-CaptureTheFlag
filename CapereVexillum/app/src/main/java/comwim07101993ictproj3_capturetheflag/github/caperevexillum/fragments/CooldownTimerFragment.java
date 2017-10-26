1b18a78261d644e97b06791aa76ffd79fb339734
joclaes321 <jo_claes321@hotmail.com>
2017-10-26 13:01:03 +0200

    cooldown fragment done

---
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/fragments/CooldownTimerFragment.java
@@ -0,0 +1,41 @@
+package comwim07101993ictproj3_capturetheflag.github.caperevexillum.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.R;
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.activities.GameActivity;
+
+
+public class CooldownTimerFragment extends Fragment {
+
+
+    TextView textView;
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_cooldown_timer,container,false);
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GameActivity gameActivity = (GameActivity) getActivity();
+        textView=(TextView)view.findViewById(R.id.cooldownTimerTextView);
+
+        textView.setText(gameActivity.cooldownLeft+" time left");
+
+    }
+
+
+}
