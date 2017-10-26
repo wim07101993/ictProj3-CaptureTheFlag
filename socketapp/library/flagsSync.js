3fa836f5d239089829194ac108cb210d2e891fe0
SultanFlorvag <michiel.vanbergen@hotmail.com>
2017-10-26 14:07:01 +0200

    update flags

--- socketapp/library/flagsSync.js
+++ socketapp/library/flagsSync.js
@@ -4,7 +4,7 @@ export default {
     sync: function(socket){
         this.staticData();
         console.log("flagsync");
-        socket.emit("syncflag",JSON.stringify(flags));
+        socket.emit("syncflag",JSON.stringify(this.flags));
     },
     staticData(){
         flags.push(new flag("234:1234:1234","orange","15"));
