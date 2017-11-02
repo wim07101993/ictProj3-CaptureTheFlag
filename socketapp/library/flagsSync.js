c11e0beeb48ea3587ec8b466c723f1cf60585605
SultanFlorvag <michiel.vanbergen@hotmail.com>
2017-11-02 20:53:10 +0100

    flagsSync Done

--- socketapp/library/flagsSync.js
+++ socketapp/library/flagsSync.js
@@ -4,9 +4,15 @@ export default {
     askFlags: function(socket){
         socket.emit("answerFlags",JSON.stringify(this.flags));
     },
-    addFlags: function(io, socket, flag){
-        let responseFlag = JSON.parse(flag);
-        this.flags.push(new Flag(responseFlag.beaconMAC , responseFlag.team , responseFlag.cooldownTime));
-        io.emit("addedFlags",JSON.stringify(this.flags));
+    addFlag: function(io, socket, flag){
+        let inputFlag = JSON.parse(flag);
+        this.flags.push(new Flag(inputFlag.beaconMAC , inputFlag.team , inputFlag.cooldownTime));
+        io.emit("addedFlag",JSON.stringify(this.flags));
+    },
+    updateFlag: function(io, socket, flag){
+        let inputFlag = JSON.parse(flag);
+        index = flags.findIndex(flag => flag.beaconMAC == inputFlag.beaconMAC);
+        flags[index] = new Flag(inputFlag.beaconMAC , inputFlag.team , inputFlag.cooldownTime);
+        io.emit("updatedFlag",JSON.stringify(this.flags));
     },
 }
