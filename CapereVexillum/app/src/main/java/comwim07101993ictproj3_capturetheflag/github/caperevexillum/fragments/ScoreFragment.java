5821806430d203b8f0f3c703eca175998aae2fb1
Georges-Grajchen <georgesjrg@gmail.com>
2017-11-07 10:47:01 +0100

    App => Scorebar + show flags. Without statemanager

--- CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/fragments/ScoreFragment.java
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/fragments/ScoreFragment.java
@@ -1,21 +1,23 @@
 package comwim07101993ictproj3_capturetheflag.github.caperevexillum.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import java.util.Vector;
 
 import comwim07101993ictproj3_capturetheflag.github.caperevexillum.R;
 import comwim07101993ictproj3_capturetheflag.github.caperevexillum.activities.GameActivity;
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.models.Flag;
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.models.Flags;
 import comwim07101993ictproj3_capturetheflag.github.caperevexillum.services.stateManager.StateManager;
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.services.stateManager.enums.StateManagerKey;
 
 
 /**
@@ -34,99 +36,81 @@ public class ScoreFragment extends Fragment{
     private StateManager stateManager;
     private View view;
     private GameActivity gameActivity;
+    private Vector<Flag> flags;
 
     private int amountFlagsGreen;
-    private int amoountFlagsRed;
-    private int scoreGreen;
-    private int scoreRed;
-    private int scoreBalance;
-
-    /* --------------------------------------------------------------- */
-    /* ------------------------- CONSTRUCTOR ------------------------- */
-    /* --------------------------------------------------------------- */
-
-    public ScoreFragment() {
-        // Required empty public constructor
-    }
+    private int amountFlagsRed;
+    private double scoreGreen;
+    private double scoreRed;
+    private double scoreBalance;
 
     /* ----------------------------------------------------------- */
     /* ------------------------- METHODS ------------------------- */
     /* ----------------------------------------------------------- */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragmen
+        view = inflater.inflate(R.layout.fragment_score,container,false);
 
-    public void addActivity(GameActivity gameActivity) {
-        this.gameActivity = gameActivity;
+        gameActivity = (GameActivity) getActivity();
+
+        amountFlagsRedView = (TextView) view.findViewById(R.id.aantalFlagsRood);
+        amountFlagsGreenView = (TextView) view.findViewById(R.id.aantalFlagsGroen);
+        scoreBalanceProgressbar = (ProgressBar) view.findViewById(R.id.scoreVerhoudingProgressBar);
+
+        stateManager = gameActivity.getStateManager();
+
+        setViewValues();
+
+        return view;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+    }
+
+    private void setViewValues(){
+        try {
+            if (stateManager !=null){
+               /*
+               //todo statemanager include
+                flags = ((Flags)stateManager.get(StateManagerKey.FLAGS)).getRegisteredFlags();
+
+                for (Flag flag: flags) {
+
+                    switch (flag.getTeam()){
+                        case "red": amountFlagsRed++;
+                            break;
+                        case "green":  amountFlagsGreen++;
+                            break;
+                        default: break;
+                    }
+
+                }*/
+            }else {
+                scoreRed = 0.01;
+                scoreGreen = 0.01;
+                amountFlagsRed = 0;
+                amountFlagsGreen = 0;
+
+
+
+            }
+            scoreBalance= scoreGreen/(scoreGreen+scoreRed)*100.0;
+
+            amountFlagsRedView.setText(amountFlagsRed+"");
+            amountFlagsGreenView.setText(amountFlagsGreen+"");
+
+            scoreBalanceProgressbar.setProgress((int)scoreBalance);
+        }catch (Exception ex){
 
         }
+
+
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_score, container, false);
-        amountFlagsRedView = (TextView) view.findViewById(R.id.aantalFlagsRood);
-        amountFlagsGreenView = (TextView) view.findViewById(R.id.aantalFlagsGroen);
-        scoreBalanceProgressbar = (ProgressBar) view.findViewById(R.id.scoreVerhoudingProgressBar);
-
-        return view;
-    }
-
-    private void fillProgressBar(){
-        scoreBalance= scoreGreen/(scoreGreen+scoreRed)*100;
-        scoreBalanceProgressbar.setProgress(scoreBalance);
-
-    }
-    private void setAmountFlags(){
-        amountFlagsRedView.setText(amoountFlagsRed);
-        amountFlagsGreenView.setText(amountFlagsGreen);
-    }
-
-
-
-    public void dummyData(){
-        amountFlagsRedView.setText("5");
-        amountFlagsRedView.setText("7");
-
-        scoreGreen = 300;
-        scoreRed = 700;
-    }
-
-    /*
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     *
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
