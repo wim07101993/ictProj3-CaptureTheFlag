01b9e4f7d4bf103e3ca3ccc96ebf4cb82c3308c8
Sven Liekens <svnlkns@gmail.com>
2017-11-12 19:52:55 +0100

    Mobile app => Add activity and layout for "Join Lobby" screen

---
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/activities/JoinLobbyActivity.java
@@ -0,0 +1,13 @@
+package comwim07101993ictproj3_capturetheflag.github.caperevexillum.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinLobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_lobby);
+    }
+}
