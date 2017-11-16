473fc3b00adf90c838b006cbac3ff1cb09dacb9a
Nick Castermans <nickcastermans@lt3170027.int.ucll.be>
2017-11-16 14:03:10 +0100

    socket => clean structure (currently not working)

--- socketapp/library/timeSync.js
+++ socketapp/library/timeSync.js
@@ -1,13 +1,13 @@
 export default {
     time:0,
     start: function(io,socket,time){
-        
+
         this.time = time;
         
-        io.emit("start",time+"");
+        io.emit("timeStart",time+"");
         let parent = this;
         setInterval(function(){parent.calculateTime(parent,io,socket)},1000)
-        
+
     },
     clientStart:function(socket){
         socket.emit("start",this.time+"");
@@ -18,10 +18,10 @@ export default {
     },
     calculateTime(parent,io,socket){
         parent.time-=0.01;
-        
+
        let seconds = Math.floor((((parent.time%1)+0.000001)*100))-40;
-       //sync every minute 
-      
+       //sync every minute
+
        if((seconds<=1) && ( seconds>=0)){
         console.log("");
             console.log("time",parent.time);
@@ -31,8 +31,8 @@ export default {
             return;
         }
         if((seconds%10)==0){
-            
+
          parent.syncTime(io,socket,parent);
-        }     
+        }
     }
 }
