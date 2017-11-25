5a7e71e48e8a1357bb0b5aeeaa31ab34cf58328a
wim <van.laer.wim@gmail.com>
2017-11-25 20:06:27 +0100

    App => created singleton factory

---
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/helpers/factories/ISingletonFactory.java
@@ -0,0 +1,12 @@
+package comwim07101993ictproj3_capturetheflag.github.caperevexillum.helpers.factories;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by wimva on 25/11/2017.
+ */
+
+public interface ISingletonFactory<T> {
+    T get(Object... args) throws IllegalAccessException, InvocationTargetException, InstantiationException;
+    T get();
+}
---
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/helpers/factories/SingletonFactory.java
@@ -0,0 +1,38 @@
+package comwim07101993ictproj3_capturetheflag.github.caperevexillum.helpers.factories;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by wimva on 25/11/2017.
+ */
+
+public abstract class SingletonFactory<T> implements ISingletonFactory<T> {
+
+    private T product;
+    private Class<T> productType;
+
+    protected SingletonFactory(Class<T> productType) {
+        this.productType = productType;
+    }
+
+    @Override
+    public T get(Object... args) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        if (product == null) {
+            try {
+                Constructor<T> constructor = productType.getConstructor(productType);
+                product = constructor.newInstance(args);
+            } catch (NoSuchMethodException e) {
+                String error = "Cannot get the constructor for type " + productType + ".";
+                throw new UnsupportedOperationException(error);
+            }
+        }
+
+        return product;
+    }
+
+    @Override
+    public T get() {
+        return product;
+    }
+}
