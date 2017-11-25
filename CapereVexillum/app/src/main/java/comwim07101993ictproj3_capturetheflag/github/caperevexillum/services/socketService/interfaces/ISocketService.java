5acc00535170507521458dfafe34dd8e3c033645
wim <van.laer.wim@gmail.com>
2017-11-25 20:50:45 +0100

    App => ISocketFactory implements INotifier

--- CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/services/socketService/interfaces/ISocketService.java
+++ CapereVexillum/app/src/main/java/comwim07101993ictproj3_capturetheflag/github/caperevexillum/services/socketService/interfaces/ISocketService.java
@@ -1,10 +1,13 @@
 package comwim07101993ictproj3_capturetheflag.github.caperevexillum.services.socketService.interfaces;
 
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.helpers.notifier.IListener;
+import comwim07101993ictproj3_capturetheflag.github.caperevexillum.helpers.notifier.INotifier;
+
 /**
  * Created by wimva on 25/11/2017.
  */
 
-public interface ISocketService<TKey> {
+public interface ISocketService<TKey> extends INotifier<IListener<INotifier, TKey>, TKey> {
     void Send(TKey key, Object value);
 
     String getServerAddress();
